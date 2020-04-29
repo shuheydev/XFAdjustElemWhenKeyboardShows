1b4e5163c172f3f31e507c02c8a614ca975a8c60
Shuhey <shuhey.dev@gmail.com>
2020-04-30 02:18:07 +0900

    Add project files.

---
+++ XFAdjustElemWhenKeyboardShows/XFAdjustElemWhenKeyboardShows/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFAdjustElemWhenKeyboardShows
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private List<string> _sampleListData;
+        public List<string> SampleListData
+        {
+            get => _sampleListData;
+            set
+            {
+                _sampleListData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.SampleListData = new List<string>
+            {
+                "item One",
+                "item Two",
+                "item Three",
+                "item Four",
+                "item Five",
+                "item Six",
+            };
+
+            this.BindingContext = this;
+        }
+    }
+}
